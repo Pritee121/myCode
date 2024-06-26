56b76db9d05044b4441f8f5c03312bdab911e560
Pritee121 <146513965+Pritee121@users.noreply.github.com>
2024-06-26 20:25:01 +0545

    python basic

---
+++ day1.py
@@ -0,0 +1,41 @@
+# me = {
+#     'name' : "Pritee",
+#     'age' : 5,
+#     'address' : "Inaruwa"
+#     }
+# if "Pritee" in me:
+#   print("name exists")
+
+# del me['age']
+# print(me)
+# print(me['name'])
+# print(me.keys())
+# print(me.values())
+
+
+# my_tupule = (1,2,3,4,5)
+# my_tupule[0] = "pritee"
+
+# fruit = ['apple', 'orange', 'mango']
+# for i in fruit:
+#     print(fruit)
+
+colors = ('red', 'green', 'blue')
+for color in colors:
+    print(colors)
+
+student_grade = {
+    'pritee' : 45,
+    'shristi' : 40,
+    'sita' : 30
+}
+
+# for student in student_grade:
+#     print(student, "scored", student_grade[student])
+
+# for student, grade in student_grade.items():
+#     print(student,"scored",grade)
+
+message = "This much for today"
+for char in message:
+    print(char)
